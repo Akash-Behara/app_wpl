69acda4d57a8c5d4800e03df28d78bd88914b5b5
Akash-Behara <behara.akash68@gmail.com>
2024-08-17 04:36:17 +0530

    tabs

--- src/components/home/ExploreGigs.jsx
+++ src/components/home/ExploreGigs.jsx
@@ -1,17 +1,68 @@
+import { useState } from "react"
+import Tabs from "../ui/Tabs"
 import ExploreGigsCard from "./ExploreGigsCard"
-import ViewBar from "./ViewBar"
+import { LayoutGrid, ListFilter, TableProperties } from "lucide-react"
+
+
+const initialTabs = [
+  {id: 'opengigs', name: 'Open Gigs', isActive: true},
+  {id: 'inreview', name: 'In Review', isActive: false},
+  {id: 'completed', name: 'Completed', isActive: false}
+]
 
 const ExploreGigs = () => {
+
+  const [tabs, setTabs] = useState(initialTabs)
+  const [selectedTab, setSelectedTab] = useState('quest')
+
+  const handleTabClick = (id) => {
+    const newTabs = tabs.map((tab) => ({
+      ...tab,
+      isActive: tab.id === id
+    }));
+    setTabs(newTabs)
+    setSelectedTab(id)
+  }
+
   return (
     <div>
         <p className="font-gridular text-primaryYellow text-[20px] leading-[24px] mb-6">Explore Gigs</p>
-        <ViewBar />
-        <div>
-            <ExploreGigsCard />
-            <ExploreGigsCard />
-            <ExploreGigsCard />
-            <ExploreGigsCard />
+        <div className="flex justify-between items-center border border-white7 rounded-[2px]">
+          <Tabs tabs={tabs} handleTabClick={handleTabClick} selectedTab={selectedTab}/>
+          <div className="flex flex-row items-center w-[200px] justify-evenly  text-white48">
+            <div className="flex flex-row justify-evenly items-center border border-white/10 rounded-lg w-[56px] h-[32px]">
+                <LayoutGrid size={12}/>
+                <div className='h-full border border-r border-white/10'></div>
+                <TableProperties className='text-primaryYellow' size={12} rotate={90}/>
+            </div>
+            <div className="flex flex-row justify-center items-center">
+                <div className="flex flex-row justify-evenly items-center border border-white/10 rounded-lg w-[89px] h-[32px]">
+                    <ListFilter size={12}/>
+                    <p className='font-gridular text-[14px] leading-[16.8px]'>Filter</p>
+                </div>
+            </div>
+          </div>
         </div>
+
+        {selectedTab === 'opengigs' && <div>
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+        </div>}
+        {selectedTab === 'inreview' &&  <div>
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+        </div>}
+        {selectedTab === 'completed' &&  <div>
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+            <ExploreGigsCard />
+        </div>}
+      
     </div>
   )
 }
